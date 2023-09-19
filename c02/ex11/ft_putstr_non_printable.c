a85ee336742b5dc65eed0c42721824b2b199292a
El Mehdi Lahrach <elahrach@e1r3p1.1337.ma>
2023-09-19 22:46:58 +0100

    its now

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -1,33 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/19 21:03:03 by elahrach          #+#    #+#             */
-/*   Updated: 2023/09/19 21:03:10 by elahrach         ###   ########.fr       */
+/*   Created: 2023/09/19 21:05:04 by elahrach          #+#    #+#             */
+/*   Updated: 2023/09/19 21:49:20 by elahrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
-{
-	unsigned int	i;
-	unsigned int	len;
-
-	i = 0;
-	len = 0;
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-	while (i < size -1 && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (len);
-}
