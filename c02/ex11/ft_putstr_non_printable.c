067cab7f7945b925e1dcc5c826162335425ca963
El Mehdi Lahrach <elahrach@e2r3p1.1337.ma>
2023-09-21 12:31:56 +0100

    c02

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,8 +6,56 @@
 /*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 21:05:04 by elahrach          #+#    #+#             */
-/*   Updated: 2023/09/19 21:49:20 by elahrach         ###   ########.fr       */
+/*   Updated: 2023/09/21 12:12:26 by elahrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	initialize_hexa(char *hexa)
+{
+	hexa[0] = '0';
+	hexa[1] = '1';
+	hexa[2] = '2';
+	hexa[3] = '3';
+	hexa[4] = '4';
+	hexa[5] = '5';
+	hexa[6] = '6';
+	hexa[7] = '7';
+	hexa[8] = '8';
+	hexa[9] = '9';
+	hexa[10] = 'a';
+	hexa[11] = 'b';
+	hexa[12] = 'c';
+	hexa[13] = 'd';
+	hexa[14] = 'e';
+	hexa[15] = 'f';
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	char	hexa[16];
+	int		i;
+
+	i = 0;
+	initialize_hexa(hexa);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+		{
+			ft_putchar(str[i]);
+		}
+		else
+		{
+			ft_putchar('\\');
+			ft_putchar(hexa[str[i] / 16]);
+			ft_putchar(hexa[str[i] % 16]);
+		}
+		i++;
+	}
+}
