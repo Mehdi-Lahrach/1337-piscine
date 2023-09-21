7068593e89d7c6b961604342a75143830ce12b0a
El Mehdi Lahrach <elahrach@e2r11p12.1337.ma>
2023-09-22 00:37:40 +0100

    ex11 ex10

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 21:05:04 by elahrach          #+#    #+#             */
-/*   Updated: 2023/09/21 12:12:26 by elahrach         ###   ########.fr       */
+/*   Updated: 2023/09/22 00:26:55 by elahrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,20 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	initialize_hexa(char *hexa)
+void	print_hexa(char c)
 {
-	hexa[0] = '0';
-	hexa[1] = '1';
-	hexa[2] = '2';
-	hexa[3] = '3';
-	hexa[4] = '4';
-	hexa[5] = '5';
-	hexa[6] = '6';
-	hexa[7] = '7';
-	hexa[8] = '8';
-	hexa[9] = '9';
-	hexa[10] = 'a';
-	hexa[11] = 'b';
-	hexa[12] = 'c';
-	hexa[13] = 'd';
-	hexa[14] = 'e';
-	hexa[15] = 'f';
+	char	*hexa;
+
+	hexa = "0123456789abcdef";
+	ft_putchar(hexa[c / 16]);
+	ft_putchar(hexa[c % 16]);
 }
 
 void	ft_putstr_non_printable(char *str)
 {
-	char	hexa[16];
 	int		i;
 
 	i = 0;
-	initialize_hexa(hexa);
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 32 && str[i] <= 126)
@@ -53,9 +40,16 @@ void	ft_putstr_non_printable(char *str)
 		else
 		{
 			ft_putchar('\\');
-			ft_putchar(hexa[str[i] / 16]);
-			ft_putchar(hexa[str[i] % 16]);
+			print_hexa(str[i]);
 		}
 		i++;
 	}
+}
+
+int main() {
+    char str[] = "Hello, \tWorld!\n"; // Example non-printable characters
+
+    ft_putstr_non_printable(str);
+
+    return 0;
 }
