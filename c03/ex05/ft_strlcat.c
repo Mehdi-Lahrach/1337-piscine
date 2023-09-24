a822589694e044c3044f1eb1cabcf859cf17dbd4
El Mehdi Lahrach <elahrach@e2r1p1.1337.ma>
2023-09-24 14:37:57 +0100

    strlcat

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,8 +6,32 @@
 /*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 21:02:47 by elahrach          #+#    #+#             */
-/*   Updated: 2023/09/23 21:02:49 by elahrach         ###   ########.fr       */
+/*   Updated: 2023/09/24 14:32:18 by elahrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	d_len;
+	unsigned int	s_len;
+	unsigned int	t_len;
 
+	d_len = 0;
+	s_len = 0;
+	while (dest[d_len] && d_len < size)
+		d_len++;
+	while (src[s_len])
+		s_len++;
+	t_len = d_len + s_len;
+	if (d_len >= size)
+		return (t_len);
+	i = d_len;
+	while (i < size - 1 && src[i - d_len])
+	{
+		dest[i] = src[i - d_len];
+		i++;
+	}
+	dest[i] = '\0';
+	return (t_len);
+}
