7b5862a76a5370a4043779f6ec3409544ed5af1c
El Mehdi Lahrach <elahrach@e1r3p11.1337.ma>
2023-09-26 20:45:04 +0100

    hello

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 20:27:05 by elahrach          #+#    #+#             */
+/*   Updated: 2023/09/26 20:43:08 by elahrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+}
