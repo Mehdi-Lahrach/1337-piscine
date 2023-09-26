d66dc2cf2dfaf75062c78ce906bff00857a3951b
El Mehdi Lahrach <elahrach@e1r3p11.1337.ma>
2023-09-26 13:57:20 +0100

    C06

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 12:49:25 by elahrach          #+#    #+#             */
+/*   Updated: 2023/09/26 13:48:03 by elahrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(int ac, char **av)
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		if (ft_strcmp(av[i], av[i + 1]) > 0)
+		{
+			tmp = av[i];
+			av[i] = av[i + 1];
+			av[i + 1] = tmp;
+			i = 0;
+		}
+		i++;
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+		return (0);
+	ft_sort_params(argc, argv);
+	while (i < argc)
+	{
+		ft_putstr(argv[i++]);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
