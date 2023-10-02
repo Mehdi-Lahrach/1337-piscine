7c7f5a4ad3b5c2ce10cecaefa81dc78a080ece49
El Mehdi Lahrach <elahrach@e2r3p6.1337.ma>
2023-10-02 20:10:43 +0100

    c08

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elahrach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 17:37:23 by elahrach          #+#    #+#             */
+/*   Updated: 2023/10/02 17:49:05 by elahrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define FALSE 0
+# define TRUE 1
+# define EVEN(x) x % 2 == 0
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
